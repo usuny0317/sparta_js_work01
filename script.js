0b0da7d6bea9b73657895d943bd83d491bf8cb0d
usuny0317 <usuny0317@naver.com>
2025-01-10 10:52:35 +0900

    api 키 값 받아서 오는 거 확인 완료! try-catch문으로 오류 잡기 시도 완료!

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-$(document).ready(function(){
+let url="https://api.themoviedb.org/3/movie/157336?api_key=2b065c456f47ab719385884224806499"
+try{
+    fetch(url)
+    .then((res) => res.json())
+    .then((data)=>{
+        console.log(data)
+    })
 
-})
+}catch(err){
+    alert("데이터를 가져오지 못했습니다...")
+}
+
